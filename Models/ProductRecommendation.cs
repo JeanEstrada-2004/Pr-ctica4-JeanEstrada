9ca3baf0fd0f9c9b6e29aaa9962628ce5fc72154
JeanEstrada-2004 <jeanestrada350@gmail.com>
2025-06-07 17:25:40 -0500

    Implementar recomendador de productos con ML.NET y vista funcional en MVC

---
+++ Models/ProductRecommendation.cs
@@ -0,0 +1,11 @@
+using Microsoft.ML.Data;
+
+namespace Práctica4_JeanEstrada.Models
+{
+    public class ProductRecommendation
+    {
+        public string ProductId { get; set; } = "";
+        public float Score { get; set; }
+    }
+
+}
